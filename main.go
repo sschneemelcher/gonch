5a5aea313fa362f4ce80a2b334f2e299f8cff677
Simon Schneemelcher <schneemelchercom@gmail.com>
2023-01-04 20:26:12 +0100

    print content of PS1 variable as prompt

--- main.go
+++ main.go
@@ -6,23 +6,31 @@ import (
 	"os"
 )
 
-func prompt(r *bufio.Reader) []string {
-	fmt.Print("$ ")
+func printPrompt() {
+	prompt := os.Getenv("PS1")
+	fmt.Print(prompt)
+}
+
+func readInput(r *bufio.Reader) string {
+
 	line, err := r.ReadString('\n')
 	if err != nil {
 		fmt.Println("Cannot read from stdin")
 		os.Exit(1)
 	}
 
-	return parseInput(line)
+	return line
 }
 
 func main() {
 	reader := bufio.NewReader(os.Stdin)
+
 	for true {
 
 		// Print prompt, read line from user input and split it into words
-		words := prompt(reader)
+		printPrompt()
+		input := readInput(reader)
+		words := parseInput(input)
 
 		stdout, stderr := runCommand(words)
 
